2b11290141cc88f8ee3c6ddf23818bf40770a8f5
leonardomenezes <leonardormenezes83@gmail.com>
2021-09-17 13:44:57 -0300

    Melhorias!

--- cypress/support/pageobjects/GithubPage.js
+++ cypress/support/pageobjects/GithubPage.js
@@ -27,23 +27,28 @@ class GithubPage {
 
     clicarBotaoSignIn(){
         cy.get(githubElements.botaoSignIn()).click()
+        cy.screenshot()
     }
 
     acessaGitHub(){
         cy.get(githubElements.acessaGitHub()).should('be.visible')
+        cy.screenshot()
     }
 
     mensagemAlerta(resultado){
         cy.get(githubElements.mensagemAlerta()).should('contain','Incorrect username or password')
         //cy.xpath(githubElements.mensagemAlerta()).should('contain',resultado)
+        cy.screenshot()
     }
     
     mensagemAlertaVerificação(resultado){
         cy.get(githubElements.mensagemAlertaVerificação()).should('contain','Device verification')
+        cy.screenshot()
     }
 
     mensagemAlertaCódigo(resultado){
         cy.get(githubElements.mensagemAlertaCódigo()).should('contain','Enter code')
+        cy.screenshot()
     }
 }
 export default GithubPage;
